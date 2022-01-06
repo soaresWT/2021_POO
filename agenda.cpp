b2af714ba8d0ce276086fa9bc8654412f9a064f6
Soares <soares@pop-os.localdomain>
2022-01-06 15:39:25 -0300

    atualizando codigo da agenda com: search parttern

--- agenda.cpp
+++ agenda.cpp
@@ -1,6 +1,16 @@
 #include<iostream> 
 #include<vector>
 #include<memory>
+#include<sstream>
+
+template <typename T>
+std::string to_string(T t) {
+    std::stringstream ss;
+    ss << t;
+    return ss.str();
+}
+
+
 using namespace std;
 class fone{
 private:
@@ -84,13 +94,82 @@ public:
            cout<<'\n'<<"Contato ja existe"<<'\n';
        }
     }
+    void apagarContato(contato contato){
+        
+        if(existe(contato)){
+            this->contatos.erase(this->contatos.begin() + this->contatos.size() - 1);
+            cout<<'\n'<<"Contato apagado com sucesso"<<'\n';
+        }
+        else{
+        cout<<'\n'<<"Contato nao existe"<<'\n';
+        }
+    }
+
+    void listarContatos(){
+        for(auto contato : this->contatos){
+            cout<<contato<<'\n';
+        }
+    }
+    /*
+    vector<contato> procurarContato(string nome){
+        vector<contato> contatos_;
+        for(auto contato : this->contatos){
+            if(contato.getNome() == nome){
+                contatos_.push_back(contato);
+                cout<<'\n'<<"Contato encontrado:"<< contato << '\n';
+            }
+        }
+        return contatos_;
+    }*/
+    void search(string pattern){
+        vector<contato> ResultadoSearch;
+        for (auto contato : this->contatos) {
+            auto texto = to_string(contato.getNome());
+            if (texto.find(pattern) != string::npos) {
+                ResultadoSearch.push_back(contato);
+            }
+        }
+        if(ResultadoSearch.size() == 0){
+            cout<<'\n'<<"Nenhum contato encontrado"<<'\n';
+        }
+        else{
+            cout<< '\n' <<ResultadoSearch.size()<<" contatos encontrados"<<'\n';
+            cout<<"Contatos encontrados:"<<'\n';
+            for (auto contato : ResultadoSearch) { 
+                cout<<contato<<'\n';
+            }
+        }
+    }
+    friend ostream& operator<<(ostream& os, const agenda& agenda) {
+        if (agenda.contatos.empty()) {
+            os << "[AGENDA VAZIA]\n";
+        } else {
+            for (auto contato : agenda.contatos) {
+                os << contato << "\n";
+            }
+        }
+        return os;
+    }
+
 };
 int main(){
     fone fone1("fone1","1112345678");
-    contato contato1("contato1",{std::make_shared<fone>(fone1)});
+    fone fone2("fone2","2212345678");
+    fone fone3("fone3","3312345678");
+    contato contato1("belinha",{std::make_shared<fone>(fone1)});
+    contato contato2("antonia",{std::make_shared<fone>(fone2)});
+    contato contato3("beto",{std::make_shared<fone>(fone3)});
     cout<<contato1<<endl;
+    cout<<contato2<<endl;
+    cout<<contato3<<endl;
     agenda agenda1;
     agenda1.addContato(contato1);
-    agenda1.addContato(contato1);
+    agenda1.addContato(contato2);
+    agenda1.addContato(contato3);
+    //agenda1.apagarContato(contato1);
+    agenda1.listarContatos();
+    agenda1.search("be");
+    cout<< '\n' << agenda1;
+
     
 }
