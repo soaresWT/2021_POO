ac3e43f5bcbd459d300dfd655696d7de365b8507
Soares <soares@pop-os.localdomain>
2022-01-06 17:02:51 -0300

    enviando herança criativa

---
+++ herançaCriativa.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+class Personagem{
+private:    
+    bool vivo = true;
+    std::string nome{""};
+    int vida{10};
+    int level{1};
+public:
+    Personagem(std::string nome = "NPC"): nome{nome}{
+        std::cout << "Personagem criado" << std::endl;
+    }
+    ~Personagem(){
+        //std::cout << "Personagem destruido" << std::endl;
+    }
+    void setNome(std::string nome){
+        this->nome = nome;
+    }
+    std::string getNome(){
+        return nome;
+    }
+    bool estaVivo(){
+        return vivo;
+    }
+    int getVida(){
+        return vida;
+    }
+    void RecebeDano(int dano){
+        if(estaVivo()){
+            vida -= dano;
+        }
+        if(vida <= 0){
+            morrer();
+        }
+    }
+    void morrer(){
+        vivo = false;
+        cout << "O personagem morreu" << endl;
+    }
+};
+class Guerreiro: public Personagem{
+private:
+    int vida = 100;
+    int ataque = 5;
+public:
+    Guerreiro(std::string nome = "Guerreiro"): Personagem{nome}{
+        std::cout << "Guerreiro criado" << std::endl;
+    }
+    ~Guerreiro(){
+        //std::cout << "Guerreiro destruido" << std::endl;
+    }
+    void atacar(Personagem* inimigo){
+        inimigo->RecebeDano(ataque);
+    }
+};
+class Mago: public Personagem{
+private:
+    int vida = 65;
+    int ataque = 15;
+public:
+    Mago(std::string nome = "Mago"): Personagem{nome}{
+        std::cout << "Mago criado" << std::endl;
+    }
+    ~Mago(){
+        //std::cout << "Mago destruido" << std::endl;
+    }
+    void atacar(Personagem* inimigo){
+        inimigo->RecebeDano(ataque);
+    }
+};
+
+int main(){
+    Personagem jorge = Personagem{"Jorge"};
+    Guerreiro guerreiro = Guerreiro{"Guerreiro"};
+    Mago mago = Mago{"Mago"};
+    guerreiro.atacar(&mago);
+    mago.atacar(&guerreiro);
+    guerreiro.atacar(&jorge);
+    cout << jorge.getVida() << endl;
+
+    return 0;
+}
